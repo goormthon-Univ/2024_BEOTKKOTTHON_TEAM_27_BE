09a3f3f11125fbf692efb8f0b255a47abf11d92a
chjung99 <cksgh1168@gmail.com>
2024-03-16 01:21:47 +0900

    init: initial project setting

---
+++ src/main/java/com/example/sodong/SodongApplication.java
@@ -0,0 +1,13 @@
+package com.example.sodong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SodongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SodongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sodong/SodongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sodong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SodongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
