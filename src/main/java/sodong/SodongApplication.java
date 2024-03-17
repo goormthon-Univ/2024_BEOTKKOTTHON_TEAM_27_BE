94ed707c3d6228b21cb0d9ab8082ff263823c4c3
chjung99 <cksgh1168@gmail.com>
2024-03-18 02:47:54 +0900

    refactor: rename package

--- src/main/java/sodong/SodongApplication.java
+++ src/main/java/sodong/SodongApplication.java
@@ -1,11 +1,10 @@
-package com.example.sodong;
+package sodong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SodongApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SodongApplication.class, args);
 	}
