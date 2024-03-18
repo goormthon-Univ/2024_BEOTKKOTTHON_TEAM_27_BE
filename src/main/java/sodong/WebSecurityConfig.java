52d21847ebf69a90ceb7d4ed3314e37303fa7c03
chjung99 <cksgh1168@gmail.com>
2024-03-19 02:44:30 +0900

    feat: security setting

---
+++ src/main/java/sodong/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package sodong;
+
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf((csrfConfig) ->
+                        csrfConfig.disable()
+                )
+                .headers((headerConfig) ->
+                        headerConfig.frameOptions(frameOptionsConfig ->
+                                frameOptionsConfig.disable()
+                        )
+                )
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers(PathRequest.toH2Console()).permitAll()
+                                .requestMatchers("login/**").permitAll()
+                                .requestMatchers("api/**").hasRole("USER")
+                                .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
