baecac8f028e1bde931273b256d0a1f7d7ed3850
chjung99 <cksgh1168@gmail.com>
2024-03-19 16:19:42 +0900

    refactor: remove login

--- src/main/java/sodong/controller/UserController.java
+++ src/main/java/sodong/controller/UserController.java
@@ -1,23 +1,23 @@
 package sodong.controller;
 
-import lombok.RequiredArgsConstructor;
+//import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import sodong.domain.User;
 import sodong.domain.UserForm;
-import sodong.repository.UserRepository;
-import sodong.security.JwtTokenProvider;
+//import sodong.repository.UserRepository;
+//import sodong.security.JwtTokenProvider;
 import sodong.service.UserService;
 
 import java.util.List;
 import java.util.Optional;
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 @RestController
 public class UserController {
-    private final PasswordEncoder passwordEncoder;
-    private final JwtTokenProvider jwtTokenProvider;
-    private final UserRepository userRepository;
+//    private final PasswordEncoder passwordEncoder;
+//    private final JwtTokenProvider jwtTokenProvider;
+//    private final UserRepository userRepository;
     private final UserService userService;
     @Autowired
     public UserController(UserService userService) {
@@ -38,13 +38,13 @@ public class UserController {
         user.setPassword(userForm.getPassword());
         userService.join(user);
     }
-    @PostMapping("/login")
-    public String login(@RequestBody Map<String, String> user) {
-        User member = userRepository.findByEmail(user.get("email"))
-                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
-        if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {
-            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
-        }
-        return jwtTokenProvider.createToken(user.getId(), member.getRoles());
-    }
+//    @PostMapping("/login")
+//    public String login(@RequestBody Map<String, String> user) {
+//        User member = userRepository.findByEmail(user.get("email"))
+//                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
+//        if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {
+//            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+//        }
+//        return jwtTokenProvider.createToken(user.getId(), member.getRoles());
+//    }
 }
