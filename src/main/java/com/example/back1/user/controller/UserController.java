a23e02f83f1be664644ed0bbe91e3437d77cd030
chjung99 <cksgh1168@gmail.com>
2024-03-20 17:14:40 +0900

    feat: chage login method GET to POST

--- src/main/java/com/example/back1/user/controller/UserController.java
+++ src/main/java/com/example/back1/user/controller/UserController.java
@@ -5,10 +5,7 @@ import com.example.back1.user.controller.dto.request.UserForm;
 import com.example.back1.user.service.UserService;
 import com.example.back1.user.service.dto.response.LoginResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -19,7 +16,7 @@ public class UserController {
     private final UserService userService;
     private final StoreService storeService;
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public LoginResponse login(@RequestBody @Valid UserForm userForm) {
         Long userId = userService.login(userForm.loginId(), userForm.password());
         Long storeId = storeService.findByUserId(userId);
